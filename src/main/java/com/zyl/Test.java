87cccf74782988f8a995c72bfc5e3d5cd514b67e
root <1067455707@qq.com>
2020-06-07 15:39:44 +0800

    dev修改

--- src/main/java/com/zyl/Test.java
+++ src/main/java/com/zyl/Test.java
@@ -7,8 +7,6 @@ package com.zyl;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("提交成功");
-        System.out.println("修改成功");
-        System.out.println("dev分支");
+        System.out.println("dev修改");
     }
 }
