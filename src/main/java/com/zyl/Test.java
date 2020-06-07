2e7644e66b594ce352f59724f40886131379107f
root <1067455707@qq.com>
2020-06-07 15:31:24 +0800

    首次提交

--- src/main/java/com/zyl/Test.java
+++ src/main/java/com/zyl/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("提交成功");
+        System.out.println("修改成功");
     }
 }
