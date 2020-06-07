3130c9c67b506a3c0c312b81039aaaf649d6189d
root <1067455707@qq.com>
2020-06-07 16:14:56 +0800

    dev修改2

--- src/main/java/com/zyl/Test.java
+++ src/main/java/com/zyl/Test.java
@@ -7,7 +7,6 @@ package com.zyl;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("master修改1");
-        System.out.println("dev修改1");
+        System.out.println("dev修改2");
     }
 }
