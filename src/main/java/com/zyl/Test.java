a0b12a2a428644bc26ee11c063df6105f933d26a
root <1067455707@qq.com>
2020-06-07 15:21:37 +0800

    Initial commit

---
+++ src/main/java/com/zyl/Test.java
@@ -0,0 +1,11 @@
+package com.zyl;
+
+/**
+ * @Author zhongyulin
+ * @Date 2020/6/7 15:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
